3c441c6f222affa68620f34e9beda4acfcbdc4a1
Альмир Абдрасулов <abdrasulov.almir@mail.ru>
2022-05-04 21:26:11 +0600

    Добавьте файлы проекта.

---
+++ Data/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Data
+{
+    class Person
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public static Person[] GetPerson()
+        {
+            var result = new Person[]
+            {
+               new Person(){ FirstName = "Алексей", LastName = "Иванов"},
+               new Person(){ FirstName = "Артём", LastName = "Смирнов"},
+               new Person(){ FirstName = "Сергей", LastName = "Кузнецов"},
+               new Person(){ FirstName = "Вадим", LastName = "Попов"},
+               new Person(){ FirstName = "Валентин", LastName = "Васильев"},
+               new Person(){ FirstName = "Данил", LastName = "Соколов"},
+               new Person(){ FirstName = "Максим", LastName = "Волков"},
+               new Person(){ FirstName = "Матвей", LastName = "Алексеев"},
+               new Person(){ FirstName = "Ирина", LastName = "Михайловна"},
+               new Person(){ FirstName = "Оксана", LastName = "Егорова"},
+               new Person(){ FirstName = "Ольга", LastName = "Козлова"},
+               new Person(){ FirstName = "Татьяна", LastName = "Степанова"},
+               
+            };
+
+            
+
+            return result;
+        }
+    }
+}
